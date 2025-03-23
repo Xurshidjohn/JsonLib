8c2d85d3c0608417ceff5d0fe54b974bfba89bfc
Xurshidjohn <xurshidjonxurshid45@gmail.com>
2025-03-23 13:09:38 +0500

    xuddi object holatida keldi masalan: <Token value='"' type='JSON_QUOTE'>
    <Token value=',' type='JSON_COMMA'>

--- src/JSON_LEXER.C
+++ src/JSON_LEXER.C
@@ -8,6 +8,28 @@
 TOKEN tokens[100] = {};
 int token_i = 0;
 
+char* get_token_type(TOKEN_TYPE token_type) {
+    switch(token_type) {
+        case JSON_COLON:
+            return "JSON_COLON";
+            break;
+        case JSON_COMMA:
+            return "JSON_COMMA";
+            break;
+        case JSON_LBRAC:
+            return "JSON_LBRAC";
+            break;
+        case JSON_RBRAC:
+            return "JSON_RBRAC";
+            break;
+        case JSON_QUOTE:
+            return "JSON_QUOTE";
+        default:
+            return "JSON_UNKNOWN";
+            break;
+    }
+}
+
 void init_json(char *JSON_DATA){
     JSON_DATA = SKIP_WHITE_SPACE(JSON_DATA);
     int i = 0;
@@ -15,37 +37,46 @@ void init_json(char *JSON_DATA){
         TOKEN token;
         switch(JSON_DATA[i]){
             case '{':
-                token.type = 4;
+            printf("%c", JSON_DATA[i]);
+                token.type = JSON_LBRAC;
                 token.value = JSON_DATA[i];
                 tokens[token_i] = token;
                 token_i++;
                 break;
             case '}':
-                token.type = 5;
+            printf("%c", JSON_DATA[i]);
+                token.type = JSON_RBRAC;
                 token.value = JSON_DATA[i];
                 tokens[token_i] = token;
                 token_i++;
                 break;
             case '"':
-                token.type = 7;
+            printf("%c", JSON_DATA[i]);
+                token.type = JSON_QUOTE;
                 token.value = JSON_DATA[i];
                 tokens[token_i] = token;
                 token_i++;
                 break;
             case ':':
-                token.type = 2;
+            printf("%c", JSON_DATA[i]);
+
+                token.type = JSON_COLON;
                 token.value = JSON_DATA[i];
                 tokens[token_i] = token;
                 token_i++;
                 break;
             case ',':
-                token.type = 3;
+            printf("%c", JSON_DATA[i]);
+                token.type = JSON_COMMA;
                 token.value = JSON_DATA[i];
                 tokens[token_i] = token;
                 token_i++;
                 break;
             default:
-                // printf("%c", JSON_DATA[i]);
+                // token.type = JSON_UNKNOWN;
+                // token.value = JSON_DATA[i];
+                // tokens[token_i] = token;
+                // token_i++;
                 break;
         }
         i++;
@@ -56,9 +87,10 @@ int main() {
     init_json("{\"username\": \"Xurshid\", \"age\": \"16\", \"isMarried\": \"false\"}");
     for(int i = 0;i < 100;i++) {
         if(tokens[i].value) {
-            printf("%c", tokens[i].value);
+            printf("\n<Token value='%c' type='%s'>\n", tokens[i].value, get_token_type(tokens[i].type));
         }
     }
+    // printf("%d", get_token_type(3));
 }
 
 // {"username": "Xurshid", "age": 16}
